a7e5679045ab05fffaef974e027fbac0553dae41
Milgam06 <milgamfruit@gmail.com>
2025-06-29 10:51:02 +0900

    fix: remove unused Service

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -3,15 +3,7 @@ import { SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable()
 export class SupabaseService extends SupabaseClient {
-  private supabase: SupabaseClient;
   constructor() {
     super(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
   }
-  // async onModuleInit() {
-  //   if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
-  //     throw new Error(
-  //       'Supabase URL and Anon Key must be set in environment variables',
-  //     );
-  //   }
-  // }
 }
