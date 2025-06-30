4eb0618baca03760a99263a55d5e6939ca36a534
Milgam06 <milgamfruit@gmail.com>
2025-06-30 14:21:49 +0900

    feat: add exception handling in SupabaseService code

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -4,6 +4,15 @@ import { SupabaseClient } from '@supabase/supabase-js';
 @Injectable()
 export class SupabaseService extends SupabaseClient {
   constructor() {
-    super(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+    const { SUPABASE_URL, SUPABASE_ANON_KEY } = process.env;
+    const hasNotSupabaseEnv = !SUPABASE_URL || !SUPABASE_ANON_KEY;
+
+    if (hasNotSupabaseEnv) {
+      throw new Error(
+        'SupabaseURL and Key must be provided in environment variables',
+      );
+    }
+
+    super(SUPABASE_URL, SUPABASE_ANON_KEY);
   }
 }
